2d03442a0585c48a917955f1dd90e27e63bc10ad
Khalid Laaroussi <klaarous@e1r2p10.1337.ma>
2023-03-25 20:05:01 +0000

    add Mandatory part

---
+++ srcs/requirements/wordpress/tools/wordpressStarter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+#cd /var/www/
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
+rm -rf *
+wp core download --allow-root
+
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+
+#mv wordpress ./var/www/
+
+sed -i  "/DB_NAME/s#.*#define( 'DB_NAME', '$DB_NAME' );#g" /var/www/wordpress/wp-config.php
+sed -i  "/DB_USER/s#.*#define( 'DB_USER', '$DB_USER' );#g" /var/www/wordpress/wp-config.php
+sed -i  "/DB_PASSWORD/s#.*#define( 'DB_PASSWORD', '$DB_PASS' );#g" /var/www/wordpress/wp-config.php
+sed -i  "/DB_HOST/s#.*#define( 'DB_HOST', '$DB_HOST' );#g" /var/www/wordpress/wp-config.php
+
+echo $WORDPRESS_DOMAIN;
+
+wp core install --url="$WORDPRESS_DOMAIN"/ --title="$WORDPRESS_TITLE" \
+    --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASS" \
+    --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email --allow-root
+
+
+wp user create $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --role=author \
+    --user_pass=$WORDPRESS_USER_PASS --allow-root
+
+/usr/sbin/php-fpm7.3 -F
